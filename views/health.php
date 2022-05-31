c23b109b62309063e633efee69bbf5c80f26cebb
Samir <samirspacetech@gmail.com>
2022-05-31 16:31:19 +0200

    [UPDATE]
    Ajout des divs d'introduction.

---
+++ views/health.php
@@ -0,0 +1,39 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bienvenue sur Happy Health, le site qui classe et affiche les statistiques sur la joie et la santé des habitants du monde entier.">
+    <meta name="robots" content="index,follow">
+    <meta property="og:title" content="Happy Health">
+    <meta property="og:description" content="Bienvenue sur Happy Health, le site qui classe et affiche les statistiques sur la joie et la santé des habitants du monde entier.">
+    <meta property="og:image" content="">
+    <meta property="og:site_name" content="Happy Health">
+    <link rel="shortcut icon" type="image/png" href="./assets/media/logo.png"/>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Happy Health</title>
+    <!--CDN TAILWIND-->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!--LINK CSS-->
+    <!--ICONIFY-->
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <!--FONT MONTSERRAT-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body class="m-0 font-['Montserrat'] bg-[#1A0245]">
+    <main class="mx-6">
+        <div class="introduction flex flex-col justify-center items-center">
+        <h1 class="text-[#FFF] font-[700] flex tracking-wide "> <span class="text-[#FEC505]" >H </span>appy <span class="text-[#44B562]"> H </span> ealth</h1>
+    <p class="font-medium text-[#fff] text-sm text-justify mb-5 ">Discover <span class="text-[#44B562]">Happiness</span> score and <span class="text-[#FEC505]">Health</span> score <span class="font-light">(life expectancy)</span> since 2015 in every country all around the world !</p>
+    <p class="font-light text-[#fff] text-justify text-[0.6rem]""><span class="font-medium">Happiness</span> scored according to GDP per Capita, Family, <span class="font-medium">Life Expectancy</span>, Freedom, Generosity, Trust Government Corruption describe the extent to which these factors contribute in evaluating the happiness in each country. </p>
+    
+        </div>
+    </main>
+     
+</body>
+</html>

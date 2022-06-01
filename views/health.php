87de090a95495347032ec7510bfb102f8a35ea70
Samir <samirspacetech@gmail.com>
2022-06-01 17:03:56 +0200

    [UPDATE]
    Ajout des div world health day et champion of health avec les icones information, trophee, lien externe et les deux fleches, la date du jour de la santé en 2022 et le drapeau de l'Iceland.

--- views/health.php
+++ views/health.php
@@ -15,9 +15,10 @@
     <meta property="og:site_name" content="Happy Health">
     <link rel="shortcut icon" type="image/png" href="./assets/media/logo.png" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="CSS/style.css">
     <title>Happy Health</title>
     <!--CDN TAILWIND-->
-    <script src="https://cdn.tailwindcss.com"></script>
+
     <!--LINK CSS-->
     <!--ICONIFY-->
     <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
@@ -30,15 +31,52 @@
 </head>
 
 <body class="m-0 font-['Montserrat'] bg-[#1A0245]">
-    <main class="mx-6">
-        <div class="introduction flex flex-col justify-center items-center">
-            <h1 class="text-[#FFF] text-[1.2rem] font-[700] flex tracking-wide mb-3"><span class="text-[#44B562]"> H </span> ealth</h1>
-            <p class="font-light text-[#fff] text-[0.5rem] text-justify mb-5 ">
-                Human <span class="font-medium">life expectancy</span> is one of the most widely used statistical indicators in the field of demographic
-                forecasting and projections, and to assess <span class="font-medium">the level of development</span> and the human development index of a
+    <main">
+        <div class="introduction flex flex-col justify-center items-center mx-6">
+            <h1 class="text-[#FFF] text-[1.5rem] font-[700] flex tracking-wide mb-3"><span class="text-[#44B562]"> H
+                </span> ealth</h1>
+            <p class="font-light text-[#fff] text-[0.8rem] text-justify mb-5 ">
+                Human <span class="font-medium">life expectancy</span> is one of the most widely used statistical
+                indicators in the field of demographic
+                forecasting and projections, and to assess <span class="font-medium">the level of development</span> and
+                the human development index of a
                 state or region of the world. </p>
-                <button type="button" class="self-start text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-[0.8rem] px-8 py-1 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Health</button>
+            <button type="button"
+                class="btn_health self-start text-[#fff] bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-[0.8rem] px-8 py-1 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Health</button>
 
+        </div>
+
+        <div class="stats w-full flex justify-around ">
+            <div
+                class="world_health_day w-5/12 bg-[#1A0245] flex justify-center items-center flex-col rounded-2xl relative mt-10">
+                <div class="circle_info flex justify-center items-center absolute w-8 h-8 rounded-full top-[-1rem]">
+                    <span class="iconify info opacity-100" data-icon="maki:information" style="color: #44b562;"></span>
+                </div>
+                <h3 class="text-[#44B562] font-bold text-[0.7rem] mb-2 mt-5">World Health Day</h3>
+                <h5 class="text-[#fff] font-medium text-[0.7rem] mb-2">7 April 2022</h5>
+                <span class="iconify text-[2rem] mb-3" data-icon="ci:external-link" style="color: #44b562;"></span>
+            </div>
+            <div
+                class="champion_health mr-5px w-5/12 bg-[#1A0245] flex justify-center items-center flex-col rounded-2xl relative mt-10">
+                <div class="circle_info flex justify-center items-center absolute w-8 h-8 rounded-full top-[-1rem]">
+                    <span class="iconify" data-icon="fa-solid:trophy" style="color: #44b562;"></span>
+                </div>
+                <h3 class="text-[#44B562] font-bold text-[0.7rem] mb-2 mt-5">Champion of health</h3>
+                <div class="info_flag flex justify-center items-center">
+                    <img class="mr-2" src="assets/media/Iceland_flag.png" alt="Flag of Iceland">
+                    <div class="info_text flex flex-col justify-center items-center text-[#fff] text-[0.7rem]">
+                        <h3>Iceland</h3>
+                        <h3 class="text-[#787878]">0,95</h3>
+                    </div>
+                </div>
+                <div class="arrow_year flex w-11/12 items-center justify-between mt-3 ">
+                    <span class="iconify cursor-pointer" data-icon="akar-icons:arrow-left" style="color: #44b562;"></span>
+                    <h3 class="text-[0.7rem] text-[#787878] mb-[0.5rem]">2022</h3>
+                    <span class="iconify cursor-pointer" data-icon="akar-icons:arrow-left" style="color: #44b562;"
+                        data-rotate="180deg"></span>
+                </div>
+
+            </div>
         </div>
     </main>
 

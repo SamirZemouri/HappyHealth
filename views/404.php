d5650d50b46db2cf6555146d92ebbccb83d43079
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-02 15:30:21 +0200

    UP
    Finalisation de la page 404, responsive.

--- views/404.php
+++ views/404.php
@@ -28,14 +28,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body class="w-full h-[702px] font-['Montserrat'] bg-[#1A0245]">
-    <main class="bg-[url('../assets/media/logo.png')] bg-no-repeat bg-bottom h-[600px] w-full flex flex-col items-center md:h-[750px]">
+    
+    <main class="h-[600px] w-full flex flex-col items-center md:h-[750px]">
         <h1 class="text-[#FEC505] font-bold text-2xl lg:text-7xl">Promo 106</h1>
         <h2 class="text-[#FEC505] font-bold text-2xl lg:text-5xl">Happiness Score</h2>
         <div class="lg:flex items-center">
             <img class="lg:hidden" src="../assets/media/404-image.png" alt="Image of a happiness graphic going down and a dying emoji">
             <img class="hidden lg:flex" src="../assets/media/404-image-desk.png" alt="Picture of a happiness graphic going down and a dying emoji">
             <div class="flex flex-col items-center">
-                <span class="text-[#DF6462] font-bold text-5xl lg:text-7xl">Rank 404</span>
+                <span class="text-[#DF6462] font-bold text-5xl lg:text-7xl">Rank 4<span class="text-[#44B562]">0</span>4</span>
+                <p class="text-[#DF6462] font-bold">Connection is no more !</p>
                 <a class="text-[#fff] underline lg:text-2xl" href="home.php">Click here to go back</a>
             </div>
         </div>

dd7ed6dca789d3a9278bcf12958843eb2e7fbab3
Samir <samirspacetech@gmail.com>
2022-06-03 17:06:34 +0200

    [FIX]
    Changement des valeurs 'case' dans les switch pour le router.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-// Récupération de l'URI sans les "/" avec la méthode explode.
+// Récupération de l'URI sans les "/" avec la fonction explode.
 $url = explode('/', $_SERVER['REQUEST_URI']);
+
 
 
 // Switch qui permet de rediriger l'utilisateur selon l'url entrée.
@@ -8,8 +9,8 @@ switch ($url[2]) {
     case '' :
         require __DIR__ . '/views/health.html';
         break;
-    case 'about' :
-        require __DIR__ . '/views/about.php';
+    case 'happiness' :
+        require __DIR__ . '/views/happiness.php';
         break;
     default:
         http_response_code(404);

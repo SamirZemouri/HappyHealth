39fd9d27c4b8eb9530868b041987f76f66d3405d
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-08 12:03:08 +0200

    Up
    Ajout des commentaires pour le fichier select_countrie.php

--- models/select_countries.php
+++ models/select_countries.php
@@ -1,10 +1,16 @@
 <?php
 
+// Fonction permettant de récupérer tous les pays dans la BDD
 function get_countries($conn){
+    // Requête SQL pour récupérer la colonne "country" dans la table "countries"
     $req_select_countries = "SELECT country FROM countries";
+    // Préparation de la requête
     $req_countries = $conn->prepare($req_select_countries);
+    // Execution de la requête
     $req_countries->execute();
+    // Récupération de la liste de tous les pays
     $countries = $req_countries->fetchAll(PDO::FETCH_ASSOC);
 
+    // Execution de la fonction
     return $countries;
 }

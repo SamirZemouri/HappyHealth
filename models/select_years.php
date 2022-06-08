d2f331bbbfa3e6d320948cf5caa6c9927ed8b791
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-08 12:17:09 +0200

    Up
    Ajout des commentaires pour le fichier select_years.php

--- models/select_years.php
+++ models/select_years.php
@@ -1,14 +1,14 @@
 <?php
 
-// Fonction permettant de récupérer tous les pays dans la BDD
+// Fonction permettant de récupérer toutes les années dans la BDD
 function get_years($conn){
-    // Requête SQL pour récupérer la colonne "country" dans la table "countries"
+    // Requête SQL pour récupérer la colonne "year" dans la table "years"
     $req_select_years = "SELECT `year` FROM years";
     // Préparation de la requête
     $req_years = $conn->prepare($req_select_years);
     // Execution de la requête
     $req_years->execute();
-    // Récupération de la liste de tous les pays
+    // Récupération de la liste de toutes les années
     $years = $req_years->fetchAll(PDO::FETCH_ASSOC);
 
     // Execution de la fonction

f752229edadcf1fc193f6103a2f1eabe4ff7ba4c
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-10 13:39:26 +0200

    Ajout
    Ajout du script permettant de séléctionner le pays ayant le meilleur health score selon l'année demandée

---
+++ models/select_champion_health.php
@@ -0,0 +1,16 @@
+<?php
+
+// Fonction permettant de récupérer tous les pays, toutes les années et toutes les valeurs dans la BDD
+function get_health_champion($conn){
+    // Requête SQL pour récupérer les colonnes "country", "year" et "health score"
+    $req_select_health_champion = "SELECT country, `year`, MAX(`value`) FROM countries_has_years INNER JOIN countries ON countries_has_years.countries_id = countries.id INNER JOIN years ON countries_has_years.years_id = years.id INNER JOIN all_values ON countries_has_years.values_health_score_id = all_values.id WHERE `year` = :year GROUP BY country ORDER BY `value` DESC LIMIT 1";
+    // Préparation de la requête
+    $req_health_champion = $conn->prepare($req_select_health_champion);
+    // Execution de la requête
+    $req_health_champion->execute();
+    // Récupération de la liste de tous les pays, toutes les années et toutes les valeurs pour le health score
+    $health_champion = $req_health_champion5->fetchAll(PDO::FETCH_ASSOC);
+
+    // Execution de la fonction
+    return $health_champion;
+}
